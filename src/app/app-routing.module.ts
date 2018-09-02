924732f1ffc1700f101ae8f6510ba1ceededead5
duanlx1 <xuanduan1905@gmail.com>
2018-09-02 14:39:47 +0700

    add site-map

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { DetailComponent } from './cosmetic/detail/detail.component';
 import { ListComponent } from './cosmetic/list/list.component';
+import { SiteMap } from './common/config/site-map';
 
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -9,11 +10,21 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgxSpinnerModule } from 'ngx-spinner';
 
 const routConfig: Routes = [
-    
-    { path: 'cosmetic/list', component: ListComponent },
-    { path: 'cosmetic/detail/:id', component: DetailComponent },
-    { path: '', redirectTo: 'cosmetic/list', pathMatch: 'full' },
-    { path: '**', redirectTo: 'cosmetic/list', pathMatch: 'full' }
+
+    // { path: SiteMap.Home, component: HomeComponent },
+    { path: SiteMap.Shop, component: ListComponent },
+    { path: SiteMap.Detail, component: DetailComponent },
+    // { path: SiteMap.Blogs, component: BlogsComponent },
+    // { path: SiteMap.BlogDetail, component: BlogDetailComponent },
+    // { path: SiteMap.Login, component: LoginComponent },
+    // { path: SiteMap.Register, component: RegisterComponent },
+    // { path: SiteMap.Contact, component: ContactComponent },
+    // { path: SiteMap.About, component: AboutComponent },
+    // { path: SiteMap.Cart, component: CartComponent },
+    // { path: SiteMap.Checkout, component: CheckoutComponent },
+    // { path: SiteMap.Favorite, component: FavoriteComponent },
+    { path: '', redirectTo: SiteMap.Shop, pathMatch: 'full' },
+    { path: '**', redirectTo: SiteMap.Shop, pathMatch: 'full' }
 ];
 
 @NgModule({
---
+++ src/app/common/config/site-map.ts
@@ -0,0 +1,18 @@
+/**
+ * Define all page of shoping frontend.
+ */
+export const enum SiteMap {
+  Home = 'home',
+  Shop = 'shop',
+  Detail = 'detail/:id',
+  Blogs = 'blogs',
+  BlogDetail = 'blockDetail/:id',
+  Login = 'login',
+  Register = 'register',
+  Contact = 'contact',
+  About = 'about',
+  Cart = 'cart',
+  Checkout = 'checkout',
+  Favorite = 'favorite',
+  PageNotFoud = '404'
+}
